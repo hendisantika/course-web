0256ccfb926fe7eb8d60eecfa1d25a055a6000d2
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 06:36:38 +0700

    Add commandToEntity in CourseMapper🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/courseweb/mapper/CourseMapper.java
+++ src/main/java/id/my/hendisantika/courseweb/mapper/CourseMapper.java
@@ -1,10 +1,14 @@
 package id.my.hendisantika.courseweb.mapper;
 
+import id.my.hendisantika.courseweb.course.Category;
+import id.my.hendisantika.courseweb.course.Course;
 import id.my.hendisantika.courseweb.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +26,19 @@ import org.springframework.stereotype.Component;
 public class CourseMapper {
     private final ModelMapper modelMapper;
     private final CategoryService categoryService;
+
+    public Course commandToEntity(CreateCourse command) {
+        log.debug("Convert 'CreateCourse' command to new course instance. ['userId': {}, 'title', {}]",
+                command.getCreatedByUserId(), command.getTitle());
+
+        Category category = this.categoryService.getById(command.getCategoryId());
+
+        Course course = modelMapper.map(command, Course.class);
+        course.setId(UUID.randomUUID());
+        course.setCategory(category);
+
+        log.debug("Course entity {} with id {} initialized.", course.getTitle(), course.getId());
+        return course;
+    }
 
 }
