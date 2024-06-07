7c6421d2c935c6c12517eaafc7246c3a9f62b7ba
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 06:41:47 +0700

    Add getById in CategoryService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/courseweb/service/CategoryService.java
+++ src/main/java/id/my/hendisantika/courseweb/service/CategoryService.java
@@ -1,10 +1,14 @@
 package id.my.hendisantika.courseweb.service;
 
+import id.my.hendisantika.courseweb.course.Category;
 import id.my.hendisantika.courseweb.mapper.CategoryMapper;
 import id.my.hendisantika.courseweb.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +28,24 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final CategoryMapper mapper;
 
+    /**
+     * Returns the instance of a category by given id.
+     *
+     * @param id the given id.
+     * @return category instance.
+     */
+    public Category getById(UUID id) {
+        Assert.notNull(id, "The given id must not be null!");
+
+        log.debug("Try to get Category with id {}", id);
+        Category category = this.categoryRepository.getOne(id);
+
+        if (category == null) {
+            log.error("Category with id {} does not exist.", id);
+            return null;
+        }
+
+        log.debug("Category {} with id {} was found.", category.getTitle(), id);
+        return category;
+    }
 }
