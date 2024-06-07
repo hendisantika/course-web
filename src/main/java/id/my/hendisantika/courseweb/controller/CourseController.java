4441200f41625f5a090e42b76aecf14448c41b02
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 06:46:47 +0700

    Add addCourse in CourseController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/courseweb/controller/CourseController.java
+++ src/main/java/id/my/hendisantika/courseweb/controller/CourseController.java
@@ -1,12 +1,24 @@
 package id.my.hendisantika.courseweb.controller;
 
+import id.my.hendisantika.courseweb.command.CreateCourse;
+import id.my.hendisantika.courseweb.course.Course;
 import id.my.hendisantika.courseweb.event.CourseCreated;
 import id.my.hendisantika.courseweb.mapper.CourseMapper;
 import id.my.hendisantika.courseweb.service.CategoryService;
+import id.my.hendisantika.courseweb.service.CourseService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.net.URI;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +39,19 @@ public class CourseController {
     private final CategoryService categoryService;
     private final CourseMapper mapper;
     private final Flux<CourseCreated> events;
+
+    @CrossOrigin
+    @PostMapping("/course")
+    ResponseEntity<UUID> addCourse(@RequestBody @Valid CreateCourse command) {
+        log.info("Create new course request received. [title: {}]", command.getTitle());
+
+        try {
+            Course course = this.courseService.createCourse(command);
+            return ResponseEntity.created(URI.create("/course/" + course.getId().toString())).body(course.getId());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
