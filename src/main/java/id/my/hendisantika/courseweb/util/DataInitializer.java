61942f1b06caeb94e6abcd9b08828a1094dfd233
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 06:49:37 +0700

    Add createExampleCourses in DataInitializer🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/courseweb/util/DataInitializer.java
+++ src/main/java/id/my/hendisantika/courseweb/util/DataInitializer.java
@@ -1,6 +1,7 @@
 package id.my.hendisantika.courseweb.util;
 
 import id.my.hendisantika.courseweb.course.Category;
+import id.my.hendisantika.courseweb.course.Course;
 import id.my.hendisantika.courseweb.repository.CategoryRepository;
 import id.my.hendisantika.courseweb.repository.CourseRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -57,5 +59,19 @@ public class DataInitializer implements ApplicationListener<ApplicationReadyEven
         });
 
         this.createExampleCourses(categories);
+    }
+
+    private void createExampleCourses(List<Category> categories) {
+        List<Course> courses = new ArrayList<>() {
+            {
+                add(new Course("Outdoor Bootcamp", categories.get(0), UUID.randomUUID(), 60L));
+                add(new Course("Hurricane Bootcamp", categories.get(0), UUID.randomUUID(), 45L));
+                add(new Course("Six Pack Workout", categories.get(3), UUID.randomUUID(), 45L));
+                add(new Course("XXL Legs Workout", categories.get(4), UUID.randomUUID(), 90L));
+            }
+        };
+
+        this.courseRepository.saveAll(courses);
+        log.debug("Sample courses created.");
     }
 }
