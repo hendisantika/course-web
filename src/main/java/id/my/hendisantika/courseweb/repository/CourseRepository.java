6a4518a90d44949bf55ba4340b5d71da6c1076b5
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 06:33:43 +0700

    Add CourseRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/courseweb/repository/CourseRepository.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.courseweb.repository;
+
+import id.my.hendisantika.courseweb.course.Category;
+import id.my.hendisantika.courseweb.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : course-web
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/8/24
+ * Time: 06:33
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CourseRepository extends JpaRepository<Course, UUID> {
+    // add custom queries here
+    List<Course> findCoursesByCategory(Category category);
+}
