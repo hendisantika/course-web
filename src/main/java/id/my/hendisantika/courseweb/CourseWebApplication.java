6340d3164bba0903f07f65c78294b6f2b00cdd2b
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 06:27:57 +0700

    Create Course Service🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/courseweb/CourseWebApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.courseweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/courseweb/CourseWebApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.courseweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
