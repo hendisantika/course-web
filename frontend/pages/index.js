7642537defe412c551a22787439e95d4c2836a28
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 07:03:16 +0700

    Add useEffect in index.js🫸🌀✏️📗🐧🐳

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 import getConfig from 'next/config';
 
@@ -8,4 +8,28 @@ const {API_HOST} = publicRuntimeConfig;
 const Index = ({initialCourses}) => {
     const [courses, setCourses] = useState(initialCourses);
 
+    useEffect(() => {
+        let eventSource = new EventSource(`/api/course`);
+        eventSource.onopen = (e) => {
+            console.log('listen to api-sse endpoint', e)
+        };
+
+        eventSource.onmessage = (e) => {
+            const course = JSON.parse(e.data);
+
+            if (!courses.includes(course)) {
+                setCourses(courses => [...courses, course]);
+            }
+        };
+
+        eventSource.onerror = (e) => {
+            console.log('error', e)
+        };
+
+        // returned function will be called on component unmount
+        return () => {
+            eventSource.close();
+            eventSource = null;
+        }
+    }, [])
 }
