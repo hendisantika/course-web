c4e9f9aefc434b38ba88c0d9f23cb51cae9ad9b8
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 07:16:19 +0700

    Need to fix🫸🌀✏️📗🐧🐳

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// module.exports = {
+//     publicRuntimeConfig: {
+//         API_HOST: process.env.API_HOST,
+//     },
+// }
+
 export default nextConfig;
