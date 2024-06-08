9d4828a8988453cdddd668cbb93b235c359f8b44
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 07:01:35 +0700

    Add html in course.js🫸🌀✏️📗🐧🐳

--- frontend/pages/course.js
+++ frontend/pages/course.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 
 import getConfig from 'next/config';
+import Link from "next/link";
 
 const {publicRuntimeConfig} = getConfig();
 const {API_HOST} = publicRuntimeConfig;
@@ -51,4 +52,36 @@ const NewCourse = ({categories, userId}) => {
     const categoryOptions = categories.map(c => {
         return <option key={c.id} value={c.id}>{c.title}</option>
     });
+
+    return <div>
+        <h1>Create New Course</h1>
+        <Link href="/">
+            <button>
+                <a>All Courses</a>
+            </button>
+        </Link>
+        {error && <h3>{error}</h3>}
+        {saved && <h3>Congrats! The course '{course.title}' was saved successfully.</h3>}
+        {!saved && <form onSubmit={handleSubmit}>
+            <label htmlFor="title">Course Title</label>
+            <input type="text" id="title" name="title" placeholder="Course Title"
+                   value={course.title || ''} onChange={handleChange}/>
+
+            <label htmlFor="duration">Duration</label>
+            <input type="text" id="duration" name="duration" placeholder="Course Duration in Minutes"
+                   value={course.duration || ''} onChange={handleChange}/>
+
+            <label htmlFor="category">Course Category</label>
+            <select id="category" name="categoryId" value={course.categoryId || ''} onChange={handleChange}>
+                {categoryOptions}
+            </select>
+
+            <label htmlFor="description">Description</label>
+            <textarea rows="10" id="description" name="description" placeholder="Description"
+                      value={course.description || ''} onChange={handleChange}/>
+
+            <input type="submit" value="Submit"/>
+        </form>}
+
+    </div>
 }
