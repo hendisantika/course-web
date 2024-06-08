6ce50cef7e79d3b12fa0e91d17e8b369e80f320f
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 07:05:59 +0700

    Add sseMiddleware in index.js🫸🌀✏️📗🐧🐳

--- frontend/pages/api/course/index.js
+++ frontend/pages/api/course/index.js
@@ -4,3 +4,22 @@ import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/post
 const API_HOST = process.env.API_HOST;
 
 const handler = nextConnect();
+
+const sseMiddleware = (req, res, next) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.flushHeaders();
+
+    const flushData = (data) => {
+        const sseFormattedResponse = `data: ${data}\n\n`;
+        res.write("event: message\n");
+        res.write(sseFormattedResponse);
+        res.flush();
+    }
+
+    Object.assign(res, {
+        flushData
+    });
+
+    next();
+}
