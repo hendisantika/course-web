d7185499343a333a2defb99c98bd41c2fe2c9d32
hendisantika <hendisantika@yahoo.co.id>
2024-06-08 07:06:36 +0700

    Add post in index.js🫸🌀✏️📗🐧🐳

--- frontend/pages/api/course/index.js
+++ frontend/pages/api/course/index.js
@@ -46,3 +46,29 @@ const stream = async (req, res) => {
         res.end();
     });
 }
+
+// Stream API Data
+handler.get(sseMiddleware, stream);
+
+handler.post(async (req, res) => {
+    console.log("api call");
+
+    const course = req.body.course;
+
+    await fetch(`${API_HOST}/course`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(course),
+    }).then(r => {
+        if (r.status === 201) {
+            res.json({message: "ok"});
+        } else {
+            res.status(r.status).json({status: r.status, message: r.statusText})
+        }
+    });
+});
+
+export default handler;
